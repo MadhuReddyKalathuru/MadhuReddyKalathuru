cf49f109a395d9829744aaa63016547f12f05831
Madhu <kalathuru.msreddy@gmail.com>
2023-07-13 12:42:55 -0400

    Dummy

--- app/src/main/java/com/msreddy/kotlinActivityLifeCycle/MainActivity.kt
+++ app/src/main/java/com/msreddy/kotlinActivityLifeCycle/MainActivity.kt
@@ -2,10 +2,16 @@ package com.msreddy.kotlinActivityLifeCycle
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        dummy()
+    }
+    fun dummy()
+    {
+        Log.d("TAG", "dummy: ")
     }
 }
